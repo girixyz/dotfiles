3d9c06f66cc24b6be485b95bcd354a8788f2da46
girixyz <girishanb2@gmail.com>
2026-06-08 19:52:21 +0530

    update dotfiles

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,7 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
   export LC_ALL="en_IN.utf8"
   export LANG="en_IN.utf8"
 
-  function push_dots
+  function pushdots
     cd ~/dotfiles
     git add .
     # If you don't type a message, it defaults to "update dotfiles"
@@ -13,4 +13,15 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
     end
     git push
     cd -
+end
+
+function turbo
+    set current (cat /sys/devices/system/cpu/cpufreq/boost)
+    if test $current -eq 1
+        echo 0 | sudo tee /sys/devices/system/cpu/cpufreq/boost
+        echo "Turbo boost OFF"
+    else
+        echo 1 | sudo tee /sys/devices/system/cpu/cpufreq/boost
+        echo "Turbo boost ON"
+    end
 end
