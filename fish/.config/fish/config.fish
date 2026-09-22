05112d173558d795ebded9a76aa41b39c457e02b
girixyz <girishanb2@gmail.com>
2026-09-22 23:24:55 +0530

    yazi and other small configs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,24 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
-  export LC_ALL="en_IN.utf8"
-  export LANG="en_IN.utf8"
+export LC_ALL="en_IN.utf8"
+export LANG="en_IN.utf8"
 
-  function pushdots
+set -gx _JAVA_AWT_WM_NONREPARENTING 1 #jdownloader
+set -gx _JAVA_OPTIONS "-Dsun.java2d.uiScale=2" #jdownloader scaling
+
+#neovim default for yazi 
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+
+function y #yazi default function 
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    command yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+        builtin cd -- "$cwd"
+    end
+    command rm -f -- "$tmp"
+end
+
+function pushdots #function to push my dotfiles to github
     cd ~/dotfiles
     git add .
     if test -n "$argv"
@@ -14,7 +30,7 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
     cd -
 end
 
-function turbo
+function turbo #turbo boost function
     switch "$argv[1]"
         case on
             echo 0 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo >/dev/null
@@ -27,7 +43,7 @@ function turbo
         case qm
             set state (cat /sys/devices/system/cpu/intel_pstate/no_turbo)
 
-            if test "$state" = "0"
+            if test "$state" = 0
                 echo "Turbo Boost: ON"
             else
                 echo "Turbo Boost: OFF"
@@ -37,4 +53,3 @@ function turbo
             echo "Usage: turbo [on|off|status]"
     end
 end
-
