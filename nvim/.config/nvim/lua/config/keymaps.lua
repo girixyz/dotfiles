9a0055f63c6d58852f3cd0c57f4d97a0991acfe2
girixyz <girishanb2@gmail.com>
2026-09-23 20:44:15 +0530

    test push

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -3,12 +3,3 @@
 -- Add any additional keymaps here
 
 -- Run the current Python file instantly using a floating terminal
-vim.keymap.set("n", "<leader>rp", function()
-  if vim.bo.filetype == "python" then
-    local filename = vim.fn.expand("%")
-    -- Opens LazyVim's native terminal and runs Python
-    Snacks.terminal("python3 " .. filename, { interactive = true })
-  else
-    vim.notify("This shortcut is only for Python files!", vim.log.levels.WARN)
-  end
-end, { desc = "Run current Python file" })
